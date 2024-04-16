cfe9fa445d30a780c7d806681b01078021963e59
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-04-16 17:09:16 +0200

    Loader

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -4,6 +4,7 @@ import { ItemAdvert } from './ItemAdvert';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAdvert } from '../../redux/operations';
+import { Loader } from '../Loader/Loader';
 
 export const Advert = () => {
   const dispatch = useDispatch();
@@ -13,22 +14,21 @@ export const Advert = () => {
     dispatch(fetchAdvert());
   }, [dispatch]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   if (error) {
     return <div>Error: {error}</div>;
   }
 
   return (
     <section className={styles.section_advert}>
+      {loading && <Loader />}
       <ul className={styles.container_advert}>
         {advert.slice(0, 4).map(obj => (
           <ItemAdvert key={obj.id} offer={obj} />
         ))}
       </ul>
-      <button className={styles.button_load_more}>Load more</button>
+      {!loading && (
+        <button className={styles.button_load_more}>Load more</button>
+      )}
     </section>
   );
 };
