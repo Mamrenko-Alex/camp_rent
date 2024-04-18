578fa45d197880c634e6524c142cf22a614e5430
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-04-18 20:32:17 +0200

    fixed path

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Loader } from 'components/Loader/Loader';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/camp_rent">
       <Provider store={store}>
         <PersistGate loading={<Loader />} persistor={persistor}>
           <App />
